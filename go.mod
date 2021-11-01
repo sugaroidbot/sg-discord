5c188a575b62b3b15de93edcdbe0ce7ab99a285a
Srevin Saju <srevinsaju@sugarlabs.org>
2021-11-01 22:36:58 +0300

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/sugaroidbot/sg-discord
+
+go 1.17
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/sugaroidbot/sg-telegram v0.0.0-20211101070108-92537283e6ac
+	github.com/withmandala/go-log v0.1.0
+)
+
+require (
+	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/smartystreets/assertions v1.2.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/google/uuid"
+	"github.com/sugaroidbot/sg-telegram/sgapi"
+	"github.com/withmandala/go-log"
+
+	"os"
+	"strings"
+)
+
+var logger = log.New(os.Stdout)
+
+var chanMap = map[string]*sgapi.WsConn{}
+const discordMessageLimit = 1750
+
+func main() {
+	dsBotToken := os.Getenv("DISCORD_BOT_TOKEN")
+	wsEndpoint := os.Getenv("SG_DS_WS_ENDPOINT")
+
+	dg, err := discordgo.New("Bot " + dsBotToken)
+	if err != nil {
+		logger.Fatal("error creating Discord session,", err)
+		return
+	}
+	userId, err := dg.User("@me")
+	if err != nil {
+		panic(err)
+	}
+	prefix := fmt.Sprintf("<@!%s>", userId.ID)
+
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		onMessageReceiveHandler(s, m, prefix, wsEndpoint)
+	})
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err = dg.Open()
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func sendMessageAsChunks(s *discordgo.Session, message string, chanId string) {
+	if len(message) > discordMessageLimit {
+		_, err := s.ChannelMessageSend(chanId, message[:discordMessageLimit] + "...")
+		if err != nil {
+			logger.Warn(err)
+		}
+		message = message[discordMessageLimit:]
+		if len(message) > 0 {
+			sendMessageAsChunks(s, message, chanId)
+		}
+	} else {
+		s.ChannelMessageSend(chanId, message)
+	}
+
+}
+
+
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func onMessageReceiveHandler(s *discordgo.Session, m *discordgo.MessageCreate, prefix string, wsEndpoint string) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	fmt.Println(m.Content, prefix)
+	if !strings.HasPrefix(m.Content, prefix) {
+		return
+	}
+
+	s.ChannelTyping(m.ChannelID)
+	v, ok := chanMap[m.ChannelID]
+	if !ok || v == nil {
+		uid := uuid.New()
+		scheme := "wss"
+
+		// use ws:// for localhost, and similar ones
+		if strings.HasPrefix(wsEndpoint, "0.0.0.0") || strings.HasPrefix(wsEndpoint, "127.0.0.1") || strings.HasPrefix(wsEndpoint, "localhost") {
+			scheme = "ws"
+		}
+		wsCon, err := sgapi.New(sgapi.Instance{Endpoint: fmt.Sprintf("%s://%s", scheme, wsEndpoint)}, uid)
+		if err != nil {
+			logger.Warn(err)
+			return
+		}
+		v = wsCon
+		chanMap[m.ChannelID] = wsCon
+
+		go func() {
+			err := sgapi.Listen(wsCon, func(resp string) {
+				if resp == "" {
+					// skip empty responses
+					return
+				}
+				sendMessageAsChunks(s, resp, m.ChannelID)
+				/*
+				msg := tgbotapi.NewMessage(Chat.ID, resp)
+
+				if strings.Contains(resp, "<sugaroid:yesno>") {
+					msg.Text = strings.Replace(resp, "<sugaroid:yesno>", "", -1)
+					msg.ReplyMarkup = keyboards["sugaroid:yesno"]
+				}
+				msg.ParseMode = tgbotapi.ModeHTML
+
+				_, err := bot.Send(msg)
+				if err != nil {
+					logger.Warn(err)
+				}*/
+			})
+			if err != nil {
+				logger.Warn(err)
+				chanMap[m.ChannelID] = nil
+				return
+			}
+		}()
+	}
+
+	logger.Infof("[#%s][%s] %s", m.ChannelID, m.Author.ID, m.Content)
+
+	err := sgapi.Send(v, strings.TrimSpace(strings.TrimPrefix(m.Content, prefix)))
+	if err != nil {
+		logger.Warn(err)
+		chanMap[m.ChannelID] = nil
+		return
+	}
+}
